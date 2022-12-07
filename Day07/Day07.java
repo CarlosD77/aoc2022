a0307e9f6a67e39db60937769fa9d4050d71dc9b
CarlosD77 <112511643+CarlosD77@users.noreply.github.com>
2022-12-07 13:36:19 -0600

    Update Day07.java

---
+++ Day07/Day07.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Day07{
+    public static void part1() throws IOException{
+        Scanner in = new Scanner(new File("sampleInput.txt"));
+        
+    }
+
+    public static void part2() throws IOException{
+
+    }
+
+    public static void main(String[] args){
+        try{ 
+            part1();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
