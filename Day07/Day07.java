36b947a0aa0414fed9a0467b46559c7effdf44b3
CarlosD77 <carlosdoble710@gmail.com>
2022-12-06 23:18:00 -0600

    Update Day07.java

---
+++ Day07/Day07.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Day07 {
+    public static void part1() throws IOException {
+
+    }
+
+    public static void part2() throws IOException {
+
+    }
+
+    public static void main(String[] args){
+        try {
+            part1();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
