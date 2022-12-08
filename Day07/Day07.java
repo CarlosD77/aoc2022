b67313e6359f339bd2aeb8ca9e42ff00b264d252
CarlosD77 <carlosdoble710@gmail.com>
2022-12-07 23:13:09 -0600

    Day07 Complete

--- Day07/Day07.java
+++ Day07/Day07.java
@@ -5,15 +5,20 @@ import java.util.ArrayList;
 
 class Directory {
     private String name;
-    private int size;
     private ArrayList<SimpleFile> containedFiles;
     private ArrayList<Directory> containedDirectories;
     private Directory parent;
     public Directory(String name){
         this.name = name;
-        this.size = 0;
         this.containedFiles = new ArrayList<SimpleFile>();
         this.containedDirectories = new ArrayList<Directory>();
+    }
+
+    public Directory(String name, Directory parent){
+        this.name = name;
+        this.containedFiles = new ArrayList<SimpleFile>();
+        this.containedDirectories = new ArrayList<Directory>();
+        this.parent = parent;
     }
 
 
@@ -21,7 +26,14 @@ class Directory {
         return this.name;
     }
     public int getSize(){
-        return this.getSize();
+        int size = 0;
+        for(Directory d : this.containedDirectories){
+            size += d.getSize();
+        }
+        for(SimpleFile f : this.containedFiles){
+            size += f.getSize();
+        }
+        return size;
     }
     public Directory getParent(){
         return this.parent;
@@ -34,7 +46,7 @@ class Directory {
     }
     public void addFile(SimpleFile f){
         this.containedFiles.add(f);
-        this.size += f.getSize();
+        
     }
 
     public void addDirectory(Directory d){
@@ -42,6 +54,13 @@ class Directory {
     }
     public void setParent(Directory d){
         this.parent = d;
+    }
+
+    public void addAllToDirectoryList(ArrayList<Directory> l){
+        l.add(this);
+        for(Directory d : this.containedDirectories){
+            d.addAllToDirectoryList(l);
+        }
     }
 
 }
@@ -67,19 +86,89 @@ public class Day07 {
 
     
     public static void part1() throws IOException {
-        Scanner in = new Scanner(new File("sampleInput.txt"));
+        Scanner in = new Scanner(new File("input.txt"));
+        ArrayList<Directory> directoryList = new ArrayList<Directory>();
+        Directory largest = new Directory("/");
+        Directory current = largest;
+        in.nextLine();
         while(in.hasNext()){
             String[] line = in.nextLine().split(" ");
+            if(line[0].equals("$")){
+                if(line[1].equals("cd")){
+                    if(line[2].equals("..")){
+                        current = current.getParent();
+                    } else if(line[2].equals("/")){
+                        current = largest;
+                    } else {
+                        for(Directory d : current.getContainedDirectories()){
+                            if(d.getName().equals(line[2])){
+                                current = d;
+                                break;
+                            }
+                        }
+                    }
+                }
+            } else if(line[0].equals("dir")){
+                current.getContainedDirectories().add(new Directory(line[1],current));
+            } else {
+                current.addFile(new SimpleFile(line[1], Integer.valueOf(line[0])));
+            }
         }
+        largest.addAllToDirectoryList(directoryList);
+        int total = 0;
+        for(Directory d : directoryList){
+            int size = d.getSize();
+            if(size < 100000){
+                total += size;
+            }
+        }
+        System.out.println(total);
     }
 
     public static void part2() throws IOException {
-
+        Scanner in = new Scanner(new File("input.txt"));
+        ArrayList<Directory> directoryList = new ArrayList<Directory>();
+        Directory largest = new Directory("/");
+        Directory current = largest;
+        in.nextLine();
+        while(in.hasNext()){
+            String[] line = in.nextLine().split(" ");
+            if(line[0].equals("$")){
+                if(line[1].equals("cd")){
+                    if(line[2].equals("..")){
+                        current = current.getParent();
+                    } else if(line[2].equals("/")){
+                        current = largest;
+                    } else {
+                        for(Directory d : current.getContainedDirectories()){
+                            if(d.getName().equals(line[2])){
+                                current = d;
+                                break;
+                            }
+                        }
+                    }
+                }
+            } else if(line[0].equals("dir")){
+                current.getContainedDirectories().add(new Directory(line[1],current));
+            } else {
+                current.addFile(new SimpleFile(line[1], Integer.valueOf(line[0])));
+            }
+        }
+        largest.addAllToDirectoryList(directoryList);
+        int difference = largest.getSize() - 40000000;
+        int smallest = 70000000;
+        for(Directory d : directoryList){
+            int size = d.getSize();
+            if(size >= difference && size < smallest){
+                smallest = size;
+            }
+        }
+        System.out.println(smallest);
     }
 
     public static void main(String[] args){
         try {
-            part1();
+            part2();
         } catch(Exception e){
             System.out.println(e);
         }
